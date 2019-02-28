063a8b38618610d31bd6bc82849fff98b4fc4638
sagarkalyan <44823440+sagarkalyan@users.noreply.github.com>
2019-02-28 11:29:09 -0800

    Update createData.sql

--- createData.sql
+++ createData.sql
@@ -1,1 +1,1 @@
-INSERT INTO Todotable(todoItem, todoDateAdded, todoStatus, todoDueBy) VALUES ("Start React Native Project", '2019-01-11', true, ''),("Start Python Project", '2019-01-21', true, '2019-03-05'),("Create an Android App", '2019-02-11', false, ''),("Create Nodejs Application", '2019-02-21', false, '2019-03-29');
+INSERT INTO Todotable(todoItem, todoDateAdded, todoStatus, todoDueBy) VALUES ("projectone", '2019-01-11', true, ''),("projecttwo", '2019-01-21', true, '2019-03-05'),("projectthree", '2019-02-11', false, ''),("projectfour", '2019-02-21', false, '2019-03-29');
